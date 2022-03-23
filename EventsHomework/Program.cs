1ec36015a5970b35ddfc3319f90b32818eed31d6
Sxizman <germanbikbow@gmail.com>
2022-03-24 00:42:28 +0300

    Create Program.cs

---
+++ EventsHomework/Program.cs
@@ -0,0 +1,25 @@
+﻿public class InputHandler
+{
+    public event EventHandler<char> OnKeyPressed;
+
+    public void Run()
+    {
+        char key = Console.ReadKey().KeyChar;
+        while (char.ToLower(key) != 'c')
+        {
+            OnKeyPressed?.Invoke(this, key);
+            key = Console.ReadKey().KeyChar;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        InputHandler sample = new InputHandler();
+        sample.OnKeyPressed +=
+            (sender, key) => Console.Write(key);
+        sample.Run();
+    }
+}

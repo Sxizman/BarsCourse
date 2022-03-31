31197f729147bbcc96ec4113b3106c56b5abe2b7
Sxizman <germanbikbow@gmail.com>
2022-04-01 02:04:01 +0300

    Added new HW

---
+++ IEnumerableHomework1/Program.cs
@@ -0,0 +1,52 @@
+﻿public interface ILogger
+{
+    public void LogInfo(string message);
+    public void LogWarning(string message);
+    public void LogError(string message, Exception ex);
+}
+
+public class LocalFileLogger<T> : ILogger
+{
+    private string _fileName;
+
+    public LocalFileLogger(string fileName)
+    {
+        _fileName = fileName;
+    }
+
+    public void LogInfo(string message)
+    {
+        File.AppendAllText(_fileName, $"[Info] : [{typeof(T).Name}] : {message}\n");
+    }
+
+    public void LogWarning(string message)
+    {
+        File.AppendAllText(_fileName, $"[Warning] : [{typeof(T).Name}] : {message}\n");
+    }
+
+    public void LogError(string message, Exception ex)
+    {
+        File.AppendAllText(_fileName, $"[Error] : [{typeof(T).Name}] : {message}. {ex.Message}\n");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var loggerInt = new LocalFileLogger<int>("logInt.txt");
+        loggerInt.LogInfo("info example");
+        loggerInt.LogWarning("warning example");
+        loggerInt.LogError("error example", new Exception("some exception"));
+
+        var loggerStr = new LocalFileLogger<string>("logStr.txt");
+        loggerStr.LogInfo("info example");
+        loggerStr.LogWarning("warning example");
+        loggerStr.LogError("error example", new Exception("some exception"));
+
+        var logger = new LocalFileLogger<ILogger>("log.txt");
+        logger.LogInfo("info example");
+        logger.LogWarning("warning example");
+        logger.LogError("error example", new Exception("some exception"));
+    }
+}
---
+++ IEnumerableHomework2/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Entity
+{
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+    public string Name { get; set; }
+}
+
+class Program
+{
+    static Dictionary<int, List<Entity>> MakeDictionary(List<Entity> list)
+    {
+        return list.Aggregate(new Dictionary<int, List<Entity>>(),
+            (dictionary, entity) =>
+            {
+                if (!dictionary.ContainsKey(entity.ParentId))
+                    dictionary[entity.ParentId] = new List<Entity>();
+                dictionary[entity.ParentId].Add(entity);
+                return dictionary;
+            });
+    }
+
+    static void Main()
+    {
+        var dictionary = MakeDictionary(new List<Entity> {
+            new Entity { Id = 1, ParentId = 0, Name = "Root entity" },
+            new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity" },
+            new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity" },
+            new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity" },
+            new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity" }
+        });
+
+        foreach (var list in dictionary)
+        {
+            Console.Write($"Key = {list.Key}, Value = List {{");
+            foreach (var entity in list.Value)
+                Console.Write($" Entity {{ Id = {entity.Id} }},");
+            Console.WriteLine("\b }");
+        }
+    }
+}
